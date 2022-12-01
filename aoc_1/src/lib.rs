7e3fa47ebe918533b45f9ab420d6a07a69cb359a
Eric <emercier@weborama.com>
2022-12-01 15:17:03 +0100

    Add an iterator way to do things

--- aoc_1/src/lib.rs
+++ aoc_1/src/lib.rs
@@ -1,10 +1,12 @@
-pub fn part_1_and_2(input_path: String, top: usize) -> anyhow::Result<u32> {
+use itertools::Itertools;
+
+pub fn loop_version(input_path: String, top: usize) -> anyhow::Result<u32> {
     let reader = std::fs::read_to_string(input_path)?;
 
     let mut totals: Vec<u32> = vec![];
     for group in reader.split("\n\n") {
         let mut total = 0u32;
-        for line in group.split("\n") {
+        for line in group.split('\n') {
             total += line.parse::<u32>()?;
         }
         totals.push(total);
@@ -15,19 +17,48 @@ pub fn part_1_and_2(input_path: String, top: usize) -> anyhow::Result<u32> {
     Ok(totals[0..top].iter().sum())
 }
 
+pub fn iterator_version(input_path: String, top: usize) -> anyhow::Result<u32> {
+    let reader = std::fs::read_to_string(input_path)?;
+    let calories: Vec<u32> = reader
+        .split("\n\n")
+        .map(|group| {
+            group
+                .split('\n')
+                .map(|calories| calories.parse::<u32>().unwrap())
+                .sum()
+        })
+        .sorted()
+        .rev()
+        .collect();
+
+    Ok(calories[0..top].iter().sum())
+}
+
 #[cfg(test)]
 mod test {
     use crate::*;
 
     #[test]
     pub fn test_part_1() {
-        let res = part_1_and_2("input_test.txt".to_owned(), 1);
+        let res = loop_version("input_test.txt".to_owned(), 1);
         assert_eq!(24000, res.unwrap());
     }
 
     #[test]
     pub fn test_part_2() {
-        let res = part_1_and_2("input_test.txt".to_owned(), 3);
+        let res = loop_version("input_test.txt".to_owned(), 3);
+        assert_eq!(45000, res.unwrap());
+    }
+
+    #[test]
+    pub fn test_part_1_iter() {
+        let res = iterator_version("input_test.txt".to_owned(), 1);
+        assert_eq!(24000, res.unwrap());
+    }
+
+    #[test]
+    pub fn test_part_2_iter() {
+        let res = iterator_version("input_test.txt".to_owned(), 3);
         assert_eq!(45000, res.unwrap());
     }
 }
--- aoc_1/src/main.rs
+++ aoc_1/src/main.rs
@@ -1,11 +1,11 @@
-use aoc_1::part_1_and_2;
+use aoc_1::{iterator_version, loop_version};
 
 fn main() {
-    if let Ok(result) = part_1_and_2("input.txt".to_owned(), 1) {
+    if let Ok(result) = loop_version("input.txt".to_owned(), 1) {
         println!("Part 1 result is {}", result);
     }
 
-    if let Ok(result) = part_1_and_2("input.txt".to_owned(), 3) {
+    if let Ok(result) = iterator_version("input.txt".to_owned(), 3) {
         println!("Part 2 result is {}", result);
     }
 }
