e5546ac3d3dc2c3ec427d1f554c7fc70767a94bb
Eric <eric@pedr0.net>
2022-12-04 21:59:48 +0100

    Use my own trait on a std type, pretty cool. Thanks Amos!

--- aoc_4/src/lib.rs
+++ aoc_4/src/lib.rs
@@ -1,51 +1,64 @@
 use std::fmt::Error;
+use std::ops::RangeInclusive;
 
-struct Area {
-    start: u32,
-    end: u32,
-}
+use anyhow::Context;
+use itertools::Itertools;
 
-impl Area {
-    fn contains(&self, other: &Area) -> bool {
-        self.start <= other.start && self.end >= other.end
+trait InclusiveRange {
+    fn contains_range(&self, other: &Self) -> bool;
+
+    fn contains_or_is_contained(&self, other: &Self) -> bool {
+        self.contains_range(other) || other.contains_range(self)
     }
 
-    fn overlap(&self, other: &Area) -> bool {
-        self.end >= other.start && self.start <= other.end
-    }
-}
+    fn contains_start(&self, other: &Self) -> bool;
 
-impl TryFrom<&str> for Area {
-    type Error = anyhow::Error;
-
-    fn try_from(value: &str) -> Result<Self, Self::Error> {
-        let (low, high) = value.split_once('-').ok_or_else(Error::default)?;
-        let (low, high): (u32, u32) = (low.parse()?, high.parse()?);
-        Ok(Area {
-            start: low,
-            end: high,
-        })
+    /// If the range contains the start of the other range OR if the other range contains part of
+    /// the current range, then we have an overlap
+    fn overlaps_or_is_overlapped(&self, other: &Self) -> bool {
+        self.contains_start(other) || other.contains_start(self)
     }
 }
 
-fn parse(line: &str) -> anyhow::Result<(Area, Area)> {
+impl<T> InclusiveRange for RangeInclusive<T>
+where
+    T: PartialOrd,
+{
+    fn contains_range(&self, other: &Self) -> bool {
+        self.contains(other.start()) && self.contains(other.end())
+    }
+
+    fn contains_start(&self, other: &Self) -> bool {
+        self.contains(other.start())
+    }
+}
+
+fn parse_range(range: &str) -> anyhow::Result<RangeInclusive<u32>> {
+    let (r1, r2) = range
+        .split('-')
+        .filter_map(|x| x.parse::<u32>().ok())
+        .collect_tuple::<(u32, u32)>()
+        .context("Could not collect tuple")?;
+    Ok(r1..=r2)
+}
+fn parse_line(line: &str) -> anyhow::Result<(RangeInclusive<u32>, RangeInclusive<u32>)> {
     let (first, second) = line.split_once(',').ok_or_else(Error::default)?;
-    Ok((first.try_into()?, second.try_into()?))
+    Ok((parse_range(first)?, parse_range(second)?))
 }
 
 pub fn part1(input_path: &str) -> anyhow::Result<u32> {
     Ok(std::fs::read_to_string(input_path)?
         .lines()
-        .filter_map(|x| parse(x).ok())
-        .filter(|(a, b)| a.contains(b) || b.contains(a))
+        .filter_map(|x| parse_line(x).ok())
+        .filter(|(a, b)| a.contains_or_is_contained(b))
         .count() as u32)
 }
 
 pub fn part2(input_path: &str) -> anyhow::Result<u32> {
     Ok(std::fs::read_to_string(input_path)?
         .lines()
-        .filter_map(|x| parse(x).ok())
-        .filter(|(a, b)| a.overlap(b))
+        .filter_map(|x| parse_line(x).ok())
+        .filter(|(a, b)| a.overlaps_or_is_overlapped(b))
         .count() as u32)
 }
 
