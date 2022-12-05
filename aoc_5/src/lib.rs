76af4e3c9fef90bba961d3c6d05c423f5be2bbe7
Eric <eric@pedr0.net>
2022-12-05 21:21:33 +0100

    Rust fmt all the way

--- aoc_5/src/lib.rs
+++ aoc_5/src/lib.rs
@@ -8,16 +8,16 @@ fn parse_initials_stacks(stacks: &str) -> anyhow::Result<HashMap<usize, Vec<char
     let mut map: HashMap<usize, Vec<char>> = HashMap::new();
     for line in stacks.lines().rev().skip(1) {
         let mut index = 1usize;
-        line.chars().collect::<Vec<char>>()
-            .chunks(4)
-            .for_each(|x| {
-                let x = x.iter().collect::<String>();
-                let mut m: Vec<&str> = x.matches(char::is_alphabetic).collect();
-                if let Some(c) = m.pop() {
-                    map.entry(index).or_default().push(c.chars().next().unwrap());
-                }
-                index += 1;
-            });
+        line.chars().collect::<Vec<char>>().chunks(4).for_each(|x| {
+            let x = x.iter().collect::<String>();
+            let mut m: Vec<&str> = x.matches(char::is_alphabetic).collect();
+            if let Some(c) = m.pop() {
+                map.entry(index)
+                    .or_default()
+                    .push(c.chars().next().unwrap());
+            }
+            index += 1;
+        });
     }
     Ok(map)
 }
@@ -30,13 +30,18 @@ struct Move {
 
 impl fmt::Debug for Move {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Move {} elements from {} to {}", self.number, self.from, self.to)
+        write!(
+            f,
+            "Move {} elements from {} to {}",
+            self.number, self.from, self.to
+        )
     }
 }
 
 /// Reads the "moves" part of the file
 fn read_moves(moves: &str) -> anyhow::Result<Vec<Move>> {
-    Ok(moves.lines()
+    Ok(moves
+        .lines()
         .map(|line| {
             let line = line.replace(char::is_alphabetic, "");
             let mut split_line = line.split_whitespace();
@@ -50,7 +55,8 @@ fn read_moves(moves: &str) -> anyhow::Result<Vec<Move>> {
 }
 
 fn moves_crates_9000(stacks: &mut HashMap<usize, Vec<char>>, m: &Move) {
-    for _ in 0..m.number { //TODO: use drain() instead?
+    for _ in 0..m.number {
+        //TODO: use drain() instead?
         let c = stacks.get_mut(&(m.from)).unwrap().pop().unwrap();
         stacks.get_mut(&(m.to)).unwrap().push(c);
     }
@@ -58,7 +64,8 @@ fn moves_crates_9000(stacks: &mut HashMap<usize, Vec<char>>, m: &Move) {
 
 fn moves_crates_9001(stacks: &mut HashMap<usize, Vec<char>>, m: &Move) {
     let mut crates = Vec::with_capacity(m.number);
-    for _ in 0..m.number { //TODO: use drain() instead?
+    for _ in 0..m.number {
+        //TODO: use drain() instead?
         let c = stacks.get_mut(&(m.from)).unwrap().pop().unwrap();
         crates.push(c);
     }
@@ -69,30 +76,38 @@ fn moves_crates_9001(stacks: &mut HashMap<usize, Vec<char>>, m: &Move) {
     }
 }
 
-
 pub fn part1(input_path: &str) -> anyhow::Result<String> {
     let input = std::fs::read_to_string(input_path)?;
-    let (initial_stacks, moves) = input
-        .split_once("\n\n").context("Could not split input")?;
+    let (initial_stacks, moves) = input.split_once("\n\n").context("Could not split input")?;
 
     let mut stacks = parse_initials_stacks(initial_stacks)?;
-    read_moves(moves)?.iter().for_each(|m| moves_crates_9000(&mut stacks, m));
+    read_moves(moves)?
+        .iter()
+        .for_each(|m| moves_crates_9000(&mut stacks, m));
 
-
-    let result = stacks.keys().sorted().map(|key| stacks[key].last().unwrap()).collect::<String>();
+    let result = stacks
+        .keys()
+        .sorted()
+        .map(|key| stacks[key].last().unwrap())
+        .collect::<String>();
 
     Ok(result)
 }
 
 pub fn part2(input_path: &str) -> anyhow::Result<String> {
     let input = std::fs::read_to_string(input_path)?;
-    let (initial_stacks, moves) = input
-        .split_once("\n\n").context("Could not split input")?;
+    let (initial_stacks, moves) = input.split_once("\n\n").context("Could not split input")?;
 
     let mut stacks = parse_initials_stacks(initial_stacks)?;
-    read_moves(moves)?.iter().for_each(|m| moves_crates_9001(&mut stacks, m));
+    read_moves(moves)?
+        .iter()
+        .for_each(|m| moves_crates_9001(&mut stacks, m));
 
-    Ok(stacks.keys().sorted().map(|key| stacks[key].last().unwrap()).collect::<String>())
+    Ok(stacks
+        .keys()
+        .sorted()
+        .map(|key| stacks[key].last().unwrap())
+        .collect::<String>())
 }
 
 #[cfg(test)]
